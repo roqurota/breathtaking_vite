0d4bd9fdd92118b30451a4eab3e94e5724f9afb0
Роман Усольцев <romanusolcev@MacBook-Pro-Roman.local>
2024-04-15 17:49:30 +0200

    fix

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ var soundsToLoad = [
   { url: './sounds/feel.mp3', name:'Feel' ,type: 'vocal' },
   { url: './sounds/breathtaking.mp3', name:'Breathtaking' ,type: 'vocal' },
   { url: './sounds/real.mp3', name:'Unreal' ,type: 'vocal' },
-  { url: './sounds/unreal.mp3' },
+  { url: './sounds/fake.mp3', name: 'Fake', type: 'vocal' },
   { url: './sounds/main_cut.mp3', name: 'main' },
   { url: './sounds/theme.mp3', name: 'theme' }
 ];
@@ -51,16 +51,6 @@ var startTime = 9950;
 sounds.forEach((sound, index) => { 
   sound.time = startTime + gap * index - (sound.offset ? sound.offset : 0); 
 });
-
-var person = [
-  { url: '/images/person/m_1.png' },
-  { url: '/images/person/m_2.png' },
-  { url: '/images/person/m_3.png' },
-  { url: '/images/person/m_4.png' },
-  { url: '/images/person/s_1.png' },
-  { url: '/images/person/s_2.png' },
-  { url: '/images/person/s_3.png' }
-];
 
 let animationTimer = null;
 let animationPause = false;
@@ -97,8 +87,11 @@ let statusLine = document.querySelector('.status-line');
 let scoreUI = document.querySelector('.score');
 let score = 0;
 
+let isMobile = mobileUser();
+let eventName = isMobile ? 'touchstart' : 'click';
+
 startBtn.addEventListener('click', runGame);
-restartBtn.addEventListener('click', restartGame);
+restartBtn.addEventListener(eventName, restartGame);
 
 function runApp() {
   appIsReady = true;
@@ -140,6 +133,7 @@ function runGame() {
       startBtn.classList.add('hidden');
   }, 400);
 
+  setFakeWord();
   toggleSoundsButtons(true);
 
   for (var i = 0; i < soundsBtn.length; i++)
@@ -149,6 +143,10 @@ function runGame() {
 
   gameTimer();
   personSing();
+
+  setTimeout(() => {
+    shuffleButtons();
+  }, 1200);
 
   restartBtn.classList.remove('hidden');
   setTimeout(() => restartBtn.classList.remove('hide'), 100);
@@ -223,6 +221,8 @@ function restartGame(force) {
       setTimeout(() => contentUI.classList.add('show'), 100);
 
       isRestarted = false;
+
+      soundsBtn.forEach(btn => btn.style = '');
   }, 1200);
 
   animationPause = false;
@@ -318,6 +318,105 @@ function showScore() {
   setTimeout(() => {
       restartBtn.classList.add('hidden');
   }, 300);
+
+  soundsBtn.forEach(btn => btn.style = '');
+}
+
+function mobileUser() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
+function setFakeWord() {
+  let fakeBtn = document.querySelector('.sound-btn[data-name="Fake"]');
+  let words = ['Lust', 'Kiss', 'Wrist', 'Feet', 'Sin', 'Things'];
+
+  fakeBtn.textContent = words[Math.floor(Math.random() * words.length)];
+}
+
+function shuffleButtons() {
+  var movesCount = 4;
+  var buttonsList = [];
+
+  soundsBtn.forEach(el => { buttonsList.push(el) });
+
+  let shuffleInterval = setInterval(() => {
+    if (!movesCount || isRestarted) {
+      clearInterval(shuffleInterval);
+      return;
+    }
+
+    let rdmA = Math.floor(Math.random() * buttonsList.length);
+    let btnA = buttonsList[rdmA];
+    buttonsList.splice(rdmA, 1);
+
+    let rdmB = Math.floor(Math.random() * buttonsList.length);
+    let btnB = buttonsList[rdmB];
+    buttonsList.splice(rdmB, 1);
+
+    let buttonA = {
+      row: btnA.getAttribute('data-row'),
+      col: btnA.getAttribute('data-col'),
+      ui: btnA,
+      transform: { x: 0, y: 0 },
+      relative: {
+        x: 0,
+        y: 0
+      }
+    }
+
+    let buttonB = {
+      row: btnB.getAttribute('data-row'),
+      col: btnB.getAttribute('data-col'),
+      transform: { x: 0, y: 0 },
+      ui: btnB
+    }
+
+    buttonA.rect = buttonA.ui.getBoundingClientRect();
+    buttonB.rect = buttonB.ui.getBoundingClientRect();
+
+    buttonA.rect.x = Math.floor(buttonA.rect.x);
+    buttonA.rect.y = Math.floor(buttonA.rect.y);
+
+    buttonB.rect.x = Math.floor(buttonB.rect.x);
+    buttonB.rect.y = Math.floor(buttonB.rect.y);
+
+    if (buttonA.rect.x < buttonB.rect.x)
+      buttonA.relative.x = -1; // lefter
+     else if (buttonA.rect.x === buttonB.rect.x)
+      buttonA.relative.x = 0; // same
+    else
+      buttonA.relative.x = 1; // righter
+
+    if (buttonA.rect.y < buttonB.rect.y)
+      buttonA.relative.y = -1; // higher
+    else if (buttonA.rect.y === buttonB.rect.y)
+      buttonA.relative.y = 0; // same
+    else
+      buttonA.relative.y = 1; // lower
+
+    buttonA.transform.x = Math.abs(buttonA.rect.x - buttonB.rect.x);
+    buttonA.transform.y = Math.abs(buttonA.rect.y - buttonB.rect.y);
+
+    buttonB.transform.x = -Math.abs(buttonB.rect.x - buttonA.rect.x);
+    buttonB.transform.y = -Math.abs(buttonB.rect.y - buttonA.rect.y);
+
+    if (buttonA.relative.x === 1) {
+      buttonA.transform.x = -buttonA.transform.x;
+      buttonB.transform.x = Math.abs(buttonB.transform.x);
+    }
+
+    if (buttonA.relative.y === 1) {
+      buttonA.transform.y = -buttonA.transform.y;
+      buttonB.transform.y = Math.abs(buttonB.transform.y);
+    }
+
+    buttonA.ui.style.transform = `translate(${buttonA.transform.x}px, ${buttonA.transform.y}px)`;
+    buttonB.ui.style.transform = `translate(${buttonB.transform.x}px, ${buttonB.transform.y}px)`;
+
+    movesCount--;
+  }, 600);
 }
 
 function visitPage() {
@@ -346,9 +445,10 @@ function soundBtnHandler(btn, index){
   var sound = sounds.find(el => el.name === buttonName);
   var preloadSound = soundsToLoad.find(el => el.name === buttonName);
 
-  sound.button = btn;
+  if (sound)
+    sound.button = btn;
 
-  btn.addEventListener('click', () => {
+  btn.addEventListener(eventName, () => {
       if (!isGame || activeVocal)
           return
 
@@ -356,14 +456,15 @@ function soundBtnHandler(btn, index){
 
       preloadSound.audio.play();
 
-      setPersonFrame(Math.floor(Math.random() * 3 + 4), 900);
+      setPersonFrame(Math.floor(Math.random() * 3 + 4), 800);
 
-      if (answerWindow && sound.name === sounds[soundAnswerIndex].name) {
-          hideHint(sound, 1);
+      if (sound)
+        if (answerWindow && sound.name === sounds[soundAnswerIndex].name) {
+            hideHint(sound, 1);
 
-          score++;
-      } else
-          hideHint(sound, 0);
+            score++;
+        } else
+            hideHint(sound, 0);
   });
 }
 
@@ -415,6 +516,14 @@ function gameTimer() {
 
               answerTimer = null;
           }, 1000)
+      }
+
+      if (soundIndex === sounds.length - 1 && !restartBtn.classList.contains('hide')) {
+        restartBtn.classList.add('hide');
+
+        setTimeout(() => {
+            restartBtn.classList.add('hidden');
+        }, 100);
       }
 
       // scoreUI.textContent = score + ' / 8';
